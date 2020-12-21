33d949dbf9f63f96b26111ba9c162517d51ed452
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-12-21 21:43:51 +0000

    Add animated version of waves

---
+++ artorithms/animated_waves.js
@@ -0,0 +1,77 @@
+let canvasWidth = 720;
+let canvasHeight = 720;
+let wavesDistance = 45;
+let pointsDistance = 10;
+let peaksNormalizer = 100;
+let chunk = 50;
+let wavesurfer = WaveSurfer.create({
+  container: 'body',
+  height: 0 // hide waveform HTML element
+});
+
+let peaks = undefined;
+
+$(document).on("change", "#fileinput", function(){
+  var file = this.files[0];
+
+  if (file) {
+    var reader = new FileReader();
+    
+    reader.onload = function (evt) {
+        var blob = new window.Blob([new Uint8Array(evt.target.result)]);
+
+        wavesurfer.loadBlob(blob);
+    };
+
+    reader.onerror = function (evt) {
+        console.error("An error ocurred reading the file: ", evt);
+    };
+
+    reader.readAsArrayBuffer(file);
+  }
+});
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  frameRate(3);
+  //noLoop();
+}
+
+function draw() {
+  background(183, 113, 61);
+  translate(0, 150);
+  console.log('Initiating...')
+  if (peaks === undefined) {
+    wavesurfer.on('ready', function () {
+      peaks = wavesurfer.backend.mergedPeaks
+    });
+  }
+  else if(peaks.length > 0) {
+    for (let i = 0; i < 10; i++) {
+      drawPeaks(peaks.splice(0, chunk), i);
+    }
+  }
+  else {
+    console.log('The end');
+  }
+}
+
+function drawPeaks(_peaks, iteration) {
+  let iterationSpace = iteration * wavesDistance;
+  let prevPoint = {
+    x: 100,
+    y: 0 + iterationSpace
+  };
+
+  for(let i = 0; i < _peaks.length; i++) {
+    stroke(255);
+    strokeWeight(2);
+    let newPoint = {
+      x: prevPoint.x + pointsDistance,
+      y: Math.abs(_peaks[i] * peaksNormalizer) + iterationSpace
+    };
+
+    line(prevPoint.x, prevPoint.y, newPoint.x, newPoint.y);
+    prevPoint = newPoint;
+  }
+}
